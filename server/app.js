0dcb0fe422cd6d68305822c08835353d9716b8e1
manish gurjar <manishboad4@gmail.com>
2026-01-11 12:48:10 +0530

    inngest fix

--- server/app.js
+++ server/app.js
@@ -1,37 +1,31 @@
 import express from "express";
-import cors from 'cors'
-import mongoconnection from "./config/mongo.js";
+import cors from "cors";
 import dotenv from "dotenv";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from "@clerk/express";
+import mongoconnection from "./config/mongo.js";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./inngest/index.js"
+import { inngest, functions } from "./inngest/index.js";
+
 dotenv.config();
 
-
-// const cors=cors();
-const app=express();
-
-const port =8080;
-
-await mongoconnection();
-
+const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
+
+// Inngest (no Clerk)
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
+// Clerk middleware
+app.use(clerkMiddleware());
 
-
-
-
-app.get("/",(req,res)=>{
-    res.send("server is working...")
+app.get("/", (req, res) => {
+  res.send("server is working...");
 });
 
-
-
-
-app.listen(port,()=>{
-    console.log(`server listing on port ${port}`);
-})
+mongoconnection().then(() => {
+  app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+  });
+});
