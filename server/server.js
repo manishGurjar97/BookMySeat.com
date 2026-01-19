851c3909954d6e6432fe1f2e2eb399663b57be79
manish gurjar <manishboad4@gmail.com>
2026-01-19 09:36:50 +0530

    all set

--- server/server.js
+++ server/server.js
@@ -1,55 +1,31 @@
-import express from 'express';
-import cors from 'cors';
-import Connectmongo from './config/mongo.js';
-// import loginRouter from "./routes/userRoute.js";
-import { clerkMiddleware } from '@clerk/express'
-import User from "./models/user.js";
-import { requireAuth } from "@clerk/express";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
+import mongoconnection from "./config/mongo.js";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js";
+
+dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-app.use(clerkMiddleware())
 
-console.log("📌 userRouter mounted");
-// app.use("/api", loginRouter);
+// Inngest (no Clerk)
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
-await Connectmongo();
-
-const port = 8080;
+// Clerk middleware
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
-  res.send("server is workinggh");
-});
-app.post("/api/login", requireAuth(), async (req, res) => {
-  console.log("🔥 /api/login HIT");
-
-  try {
-    const userId = req.auth.userId;
-
-    const { name, email, image } = req.body;
-
-    let user = await User.findById(userId);
-
-    if (!user) {
-      user = await User.create({
-        _id: userId,
-        name,
-        email,
-        image,
-      });
-    }
-
-    res.json({ success: true, user });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err.message });
-  }
+  res.send("server is working...");
 });
 
-
-
-app.listen(port, () => {
-  console.log("server is on...");
+mongoconnection().then(() => {
+  app.listen(port, () => {
+    console.log(`server listening on port ${port}`);
+  });
 });
